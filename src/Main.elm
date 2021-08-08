bc3914a086ddfe09e9e83a80d4be310e474c39e3
Hugo Mills <hugo@carfax.org.uk>
2021-08-08 18:22:53 +0100

    Display and allow update of RAID parameters
    Signed-off-by: Hugo Mills <hugo@carfax.org.uk>

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, h1, h2, button, div, span, text, label, input, br)
-import Html.Attributes exposing (attribute, class, type_, name, value)
-import Html.Events exposing (onInput)
+import Html exposing (Html, h1, h2, button, div, span, text, label, input, br,
+                      table, tr, td)
+import Html.Attributes exposing (attribute, class, type_, name, value, checked)
+import Html.Events exposing (onInput, onClick)
 
 main = Browser.document {
            init = init,
@@ -14,6 +15,7 @@ main = Browser.document {
 
 type Msg = AlterDeviceCount String
          | AlterDeviceSize Int String
+         | AlterRaidParam RaidParamType String
 
 type RaidPreset = Single
                 | RAID0
@@ -24,16 +26,19 @@ type RaidPreset = Single
                 | RAID5
                 | RAID6
 
+type RaidParamType = Copies | MinStripes | MaxStripes | Parity
+type alias RaidParams = { c: Int, slo: Int, shi: Int, p: Int }
+
 type alias Model = {
         disk_size: List Int,
-        preset_selected: Maybe RaidPreset
+        raid_level: RaidParams
     }
 
 init: () -> (Model, Cmd Msg)
 init _ = (
           {
               disk_size = [ 1000 ],
-              preset_selected = Nothing
+              raid_level = { c=1, slo=1, shi=1, p=0 }
           },
           Cmd.none
          )
@@ -49,6 +54,11 @@ update msg model =
         AlterDeviceSize dev value ->
             ({model | disk_size = update_disk_size dev value model.disk_size},
              Cmd.none)
+        AlterRaidParam which value ->
+            let
+                raid_level = update_raid_parameter which value model.raid_level
+            in
+                ({model | raid_level = raid_level}, Cmd.none)
 
 update_disk_list: String -> List Int -> List Int
 update_disk_list text_value disks =
@@ -84,6 +94,20 @@ update_one_disk_size seek value index disk =
     else
         disk
 
+update_raid_parameter param_type text_value raid_level =
+    let
+        value = Maybe.withDefault 1 (String.toInt text_value)
+    in
+        case param_type of
+            Copies ->
+                { raid_level | c = value }
+            MinStripes ->
+                { raid_level | slo = value }
+            MaxStripes ->
+                { raid_level | shi = value }
+            Parity ->
+                { raid_level | p = value }
+
 -- View
 
 view model =
@@ -94,8 +118,8 @@ view model =
              view_num_devices model.disk_size,
              div [ class "main-section" ]
                  [ h2 []  [ text "RAID levels" ],
-                   div [ class "raid-levels" ]
-                       [ view_raid_levels model.preset_selected ]
+                   div [ class "raid-params" ]
+                       [ view_raid_params model.raid_level ]
                  ],
              div [ class "main-section" ]
                  [ h2 [] [ text "Device sizes" ],
@@ -116,8 +140,26 @@ view_num_devices disks =
                ] []
         ]
 
-view_raid_levels preset =
-    div [] []
+view_raid_params level =
+    div [] [ table [] [
+                  raid_param_line "Copies:" "c" level.c Copies,
+                  raid_param_line "Min data stripes:" "slo" level.slo MinStripes,
+                  raid_param_line "Max data stripes:" "shi" level.shi MaxStripes,
+                  raid_param_line "Parity stripes:" "p" level.p Parity
+                 ]
+        ]
+
+raid_param_line label ctrl_name param_value event =
+    tr [] [ td [] [ text label ],
+            td [] [ input [ type_ "number",
+                            name ctrl_name,
+                            attribute "min" "0",
+                            attribute "max" "100",
+                            value <| String.fromInt param_value,
+                            onInput <| AlterRaidParam event
+                        ] []
+                ]
+        ]
 
 view_devices disks =
     div [] <| List.reverse <| List.indexedMap disk_to_device_line disks
@@ -137,3 +179,17 @@ disk_to_device_line i disk =
 subscriptions: Model -> Sub msg
 subscriptions model =
     Sub.none
+
+-- Model
+
+raid_presets: RaidPreset -> RaidParams
+raid_presets preset =
+    case preset of
+        Single  -> { c=1, slo=1, shi=1,   p=0 }
+        RAID0   -> { c=1, slo=2, shi=100, p=0 }
+        RAID1   -> { c=2, slo=1, shi=1,   p=0 }
+        RAID1c3 -> { c=3, slo=1, shi=1,   p=0 }
+        RAID1c4 -> { c=4, slo=1, shi=1,   p=0 }
+        RAID10  -> { c=2, slo=2, shi=100, p=0 }
+        RAID5   -> { c=1, slo=1, shi=100, p=1 }
+        RAID6   -> { c=1, slo=1, shi=100, p=2 }
